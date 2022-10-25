c0a036f946bf1fbfec096e4fc4d92e648ec60d8e
Rakib86547 <contact.antor.bd@gmail.com>
2022-10-26 01:23:15 +0600

    api setup

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const Port = process.env.PORT || 500;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
-
+const categories = require('./data/categories.json');
+const tutorials = require('./data/tutorials.json');
 app.get('/', (req, res) => {
     res.send('api is running')
 });
 
+app.get('/programing-category', (req, res) => {
+    res.send(categories)
+})
 
-app.listen(Port, () => {
-    console.log(`server side, ${Port}`)
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const select_category = tutorials.filter(tuto => tuto.category_id === id);
+    res.send(select_category);
+})
+
+app.get('/tutorials/:id', (req, res) => {
+    const id = req.params.id;
+    const program_tutorial = tutorials.find(tuto => tuto._id === id);
+    res.send(program_tutorial)
+})
+
+app.listen(port, () => {
+    console.log(`server side, ${port}`)
 })
